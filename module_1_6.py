1d9ddb3d71f5b81d88eb3eee1dcff83630fcbca7
Vasik429 <v.404.akk@gmail.com>
2024-11-21 02:51:52 +0500

    Кортежи

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+my_dict = {'Cucumbers': 20, 'Mandarins': 30, 'Tomatoes': 70}
+print(my_dict)
+print(my_dict['Cucumbers'])
+my_dict ['apples'] = 2
+print(my_dict['apples'])
+print(my_dict)
+my_dict.update ({'mushrooms': 7,
+               'Turtle': 1})
+print(my_dict)
+print(my_dict.get('Mandarins'))
+my_dict.pop ('Mandarins')
+print(my_dict)
+print(my_dict.get('Mandarins'))
+print(my_dict)
+
+my_set = {1,2,3,7,2,3,7,1,'Turtle', 0.5}
+print(my_set)
